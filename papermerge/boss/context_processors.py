803031c95eb5410f2faaef69000f77850fb31278
Eugen Ciur <eugen@django-lessons.com>
2020-01-08 20:27:16 +0100

    ASSETS VER might be None

--- papermerge/boss/context_processors.py
+++ papermerge/boss/context_processors.py
@@ -4,32 +4,19 @@ from django.conf import settings
 def static_bundle_url(request):
     """
     Static resources are deployed separately from main django app
-    and are available via /webassets/ prefix.
+    and are available via /assets/ prefix.
 
     In production static resources are also versioned.
     """
     if settings.DEBUG:
-        path_js = "/papermerge-assets/static/js/all.js"
-        path_css = "/papermerge-assets/static/css/all.css"
-        oxygen_path_js = "/oxygen-assets/static/js/all.js"
-        oxygen_path_css = "/oxygen-assets/static/css/all.css"
+        path_js = "/assets/static/js/all.js"
+        path_css = "/assets/static/css/all.css"
     else:
-        path_js = "/papermerge-assets/static/js/all.{}.js".format(
-            settings.papermerge_ASSETS_VER
-        )
-        path_css = "/papermerge-assets/static/css/all.{}.css".format(
-            settings.papermerge_ASSETS_VER
-        )
-        oxygen_path_js = "/oxygen-assets/static/js/all.{}.js".format(
-            settings.OXYGEN_ASSETS_VER
-        )
-        oxygen_path_css = "/oxygen-assets/static/css/all.{}.css".format(
-            settings.OXYGEN_ASSETS_VER
-        )
+        ver = settings.ASSETS_VER or ""
+        path_js = f"/assets/static/js/all.{ver}.js"
+        path_css = f"/assets/static/css/all.{ver}.css"
 
     return {
-        'papermerge_JS_URL': path_js,
-        'papermerge_CSS_URL': path_css,
-        'OXYGEN_JS_URL': oxygen_path_js,
-        'OXYGEN_CSS_URL': oxygen_path_css,
+        'JS_URL': path_js,
+        'CSS_URL': path_css,
     }
