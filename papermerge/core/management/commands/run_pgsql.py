febaf1438202e27c1826ebd21208fe6dfa196232
Eugen Ciur <eugen@django-lessons.com>
2020-01-16 11:38:01 +0100

    enabling FTS

--- papermerge/core/management/commands/run_pgsql.py
+++ papermerge/core/management/commands/run_pgsql.py
@@ -2,12 +2,6 @@ import os
 import logging
 from django.core.management.base import BaseCommand
 from django.apps import apps
-
-try:
-    from django_tenants.utils import get_tenant_model
-except:
-    get_tenant_model = None
-
 from django.db import connection
 
 logger = logging.getLogger(__name__)
@@ -15,7 +9,7 @@ logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
 
-    help = """Run specified SQL file in all schemas.
+    help = """Run specified SQL file.
 """
 
     def add_arguments(self, parser):
@@ -24,18 +18,11 @@ class Command(BaseCommand):
             '-f',
             help="SQL file name"
         )
-        parser.add_argument(
-            '--schema-name',
-            '-s',
-            help="Run sql command only on this schema"
-        )
 
     def run_sql(
         self,
-        tenant_name=None,
         sql_content=None
     ):
-        logger.debug(f"Executing for tenant {tenant_name}")
         with connection.cursor() as c:
             c.execute(sql_content)
 
@@ -44,7 +31,6 @@ class Command(BaseCommand):
             'file_name',
             False
         )
-        schema_name = options.get('schema_name', False)
         core_path = apps.get_app_config('core').path
 
         full_sql_file_path = os.path.join(
@@ -65,16 +51,6 @@ class Command(BaseCommand):
             logger.debug(f"No SQL content available. Aborting.")
             return
 
-        TenantModel = get_tenant_model()
-
-        if schema_name:
-            tenant_list = TenantModel.objects.filter(name=schema_name)
-        else:
-            tenant_list = TenantModel.objects.exclude(name="public")
-
-        for tenant in tenant_list:
-            connection.set_tenant(tenant)
-            self.run_sql(
-                tenant_name=tenant.name,
-                sql_content=sql_content
-            )
+        self.run_sql(
+            sql_content=sql_content
+        )
