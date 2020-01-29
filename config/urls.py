7ed522915eb60cb7063a52fe8999729e3f6b164c
Eugen Ciur <eugen@django-lessons.com>
2020-01-29 07:37:21 +0100

    Allow django to serve static assets in development

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from django.views.generic.base import RedirectView
-from django.views.i18n import JavaScriptCatalog
 
 from django.conf.urls import (
     include,
@@ -14,7 +12,6 @@ from papermerge.boss.admin import site as boss_site
 from django.conf.urls.static import static
 from django.conf import settings
 
-from allauth.account import views as allauth_views
 
 urlpatterns = [
     path('login/', boss_site.login, name="login"),
@@ -23,7 +20,18 @@ urlpatterns = [
     path('admin/', boss_site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('papermerge.core.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    # Serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
+
 
 handler400 = 'papermerge.core.views.errors.bad_request_400_custom'
 handler403 = 'papermerge.core.views.errors.permission_denied_403_custom'
--- papermerge/boss/context_processors.py
+++ papermerge/boss/context_processors.py
@@ -9,12 +9,12 @@ def static_bundle_url(request):
     In production static resources are also versioned.
     """
     if settings.DEBUG:
-        path_js = "/assets/js/papermerge.js"
-        path_css = "/assets/css/papermerge.css"
+        path_js = f"{settings.STATIC_URL}js/papermerge.js"
+        path_css = f"{settings.STATIC_URL}css/papermerge.css"
     else:
         ver = settings.ASSETS_VER or ""
-        path_js = f"/assets/js/papermerge.{ver}.js"
-        path_css = f"/assets/css/papermerge.{ver}.css"
+        path_js = f"{settings.STATIC_URL}js/papermerge.{ver}.js"
+        path_css = f"{settings.STATIC_URL}css/papermerge.{ver}.css"
 
     return {
         'JS_URL': path_js,
