149ec02fd47bb1b0ad8f0d7875b7a363abc34b11
Eugen Ciur <eugen@django-lessons.com>
2020-04-13 07:53:44 +0200

    Design of clipboard middleware

--- papermerge/middleware/clipboard.py
+++ papermerge/middleware/clipboard.py
@@ -6,8 +6,24 @@ class Clipboard:
 
     Adds a clipboard attribute to request object.
 
-    request.clipboard.
+    * Resets/Delets everything (for current user) from clipboard:
 
+        request.clipboard.reset()
+
+    * Get cliboard object for currently logged in user:
+
+        request.clipboard
+
+    * Puts node ids into the clipboard. Node might be a folder or a document
+
+        request.clipboard.put(node_ids=[1, 2, 3, 4])
+
+    * Puts page ids and their document id into the clipboard:
+
+        request.clipboard.put(document_id=1, page_nums=[1, 2, 3])
+
+    Achtung! page_nums are actual page order numbers (not their IDs), e.g.
+    page_nums = [1, 2] are first and second pages of the associated document
 
     """
 
