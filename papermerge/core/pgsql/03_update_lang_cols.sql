48f1f6f76c2e35c9482f87044b240b9edda59bc2
Eugen Ciur <eugen@django-lessons.com>
2020-04-19 14:10:58 +0200

    Papermerge DMS basically supports any language!

--- papermerge/core/pgsql/03_update_lang_cols.sql
+++ papermerge/core/pgsql/03_update_lang_cols.sql
@@ -1,6 +1,6 @@
 UPDATE core_page 
 SET text_fts=setweight(
-    to_tsvector(core_languagemap.pg_catalog, coalesce(text,'')),
+    to_tsvector(core_languagemap.pg_catalog::regconfig, coalesce(text,'')),
     'C'
 )
 FROM core_languagemap
@@ -10,7 +10,7 @@ WHERE core_languagemap.tsr_code = core_page.lang;
 UPDATE core_basetreenode 
 SET title_fts=setweight( 
     to_tsvector(
-        core_languagemap.pg_catalog,
+        core_languagemap.pg_catalog::regconfig,
         coalesce(
             regexp_replace(title, '[^[:alnum:]]', ' ', 'g'),
             ''
@@ -19,13 +19,13 @@ SET title_fts=setweight(
     'A'
 )
 FROM core_languagemap
-WHERE core_languagemap.tsr_code = core_page.lang;
+WHERE core_languagemap.tsr_code = core_basetreenode.lang;
 
 
 UPDATE core_basetreenode as node
 SET ancestors_fts = ancestors_to_tsvector(
-        core_languagemap.pg_short,
+        core_languagemap.pg_short::regconfig,
         get_ancestors(node.id)
     )
 FROM core_languagemap
-WHERE core_languagemap.tsr_code = core_page.lang;
+WHERE core_languagemap.tsr_code = node.lang;
