90114e97238e4d012c5d760b4feaf0ac3e210ab8
Eugen Ciur <eugen@django-lessons.com>
2020-04-19 10:17:52 +0200

    change code for fts triggers on page and node tables

--- papermerge/core/pgsql/01_triggers.sql
+++ papermerge/core/pgsql/01_triggers.sql
@@ -20,34 +20,27 @@ END
 $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION core_basetreenode_trigger() RETURNS trigger AS $$
-begin
-    IF new.lang = 'deu'
-    THEN
-        new.title_deu :=
-         setweight(
-            to_tsvector(
-                'pg_catalog.german', 
-                coalesce(
-                    regexp_replace(new.title, '[^[:alnum:]]', ' ', 'g'),
-                '')
-            ),
-        'A');
-      
-    ELSE
-        new.title_eng :=
-            setweight(
-                to_tsvector(
-                    'pg_catalog.english',
-                    coalesce(
-                        regexp_replace(new.title, '[^[:alnum:]]', ' ', 'g'),
-                    '')
-                ),
-                'A'
-            );
-    END IF;
+DECLARE
+    pg_catalog_name varchar(64);
+BEGIN
+    
+    SELECT core_languagemap.pg_catalog
+    INTO pg_catalog_name
+    FROM core_languagemap
+    WHERE core_languagemap.tsr_code = new.lang;
+
+    new.title_tsr :=
+     setweight(
+        to_tsvector(
+            pg_catalog_name, 
+            coalesce(
+                regexp_replace(new.title, '[^[:alnum:]]', ' ', 'g'),
+            '')
+        ),
+    'A');
 
     RETURN new;
-end
+END
 $$ LANGUAGE plpgsql;
 
 
@@ -55,8 +48,8 @@ DROP TRIGGER IF EXISTS tsvector_update_core_page ON core_page;
 CREATE TRIGGER tsvector_update_core_page BEFORE INSERT OR UPDATE
     ON core_page FOR EACH ROW EXECUTE PROCEDURE core_page_trigger();
 
-DROP TRIGGER IF EXISTS tsvector_update_core_basetreenode ON core_page;
+DROP TRIGGER IF EXISTS tsvector_update_core_basetreenode ON core_basetreenode;
 CREATE TRIGGER tsvector_update_core_basetreenode BEFORE INSERT OR UPDATE
-    ON core_page FOR EACH ROW EXECUTE PROCEDURE core_page_trigger();
+    ON core_basetreenode FOR EACH ROW EXECUTE PROCEDURE core_basetreenode_trigger();
 
 
