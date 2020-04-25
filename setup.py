5ea87289b9b7abd50b73524b1d89020f20e4c9be
Eugen Ciur <eugen@django-lessons.com>
2020-04-25 08:20:33 +0200

    refactoring

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 import os
 from setuptools import setup, find_packages
-from setuptools.command.egg_info import manifest_maker
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
