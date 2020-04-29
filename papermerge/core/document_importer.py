5374ccd81bb2b541f94fdeb5b4f4bb62fb1dbcd0
Eugen Ciur <eugen@django-lessons.com>
2020-04-29 08:40:20 +0200

    papermerge.conf file

--- papermerge/core/document_importer.py
+++ papermerge/core/document_importer.py
@@ -7,4 +7,8 @@ class DocumentImporter:
     def __init__(self, directory=settings.PAPERMERGE_IMPORTER_DIR):
 
         self.logger = logging.getLogger(__name__)
+        # importer dir
         self.directory = directory
+
+        if not self.directory:
+            raise Exception("The PAPERMERGE_IMPORTER_DIR is not set.")
