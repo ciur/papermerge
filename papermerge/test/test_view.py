e02600e5cbdd12e7543f731bc656a67e468cbd9d
Eugen Ciur <eugen@django-lessons.com>
2020-05-09 15:51:38 +0200

    testing and refactoring core functions 'core:upload'

---
+++ papermerge/test/test_view.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+from django.test import Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+User = get_user_model()
+
+
+class TestBasicUpload(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='john',
+            is_active=True,
+        )
+        self.user.set_password('test')
+        self.user.save()
+        self.client = Client()
+        self.client.login(
+            username='john',
+            password='test'
+        )
+
+    def test_basic_upload(self):
+        ret = self.client.post(
+            reverse('core:upload')
+        )
+
+        self.assertEqual(ret.status_code, 200)
