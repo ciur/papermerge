9d622a1d97098abcadd10571889c2abe889e3d4c
Eugen Ciur <eugen@django-lessons.com>
2020-05-22 08:09:03 +0200

    get_search_backend()

---
+++ papermerge/search/backends/__init__.py
@@ -0,0 +1,25 @@
+from importlib import import_module
+
+from django.core.exceptions import ImproperlyConfigured
+from django.conf import settings
+
+
+def import_backend(dotted_path):
+    try:
+        backend_module = import_module(dotted_path)
+        return backend_module.SearchBackend
+    except ImportError as e:
+        raise ImportError from e
+
+
+def get_search_backend():
+    backend = getattr(settings, 'PAPERMERGE_SEARCH_BACKEND')
+    # Try to import the backend
+    try:
+        backend_cls = import_backend(backend)
+    except ImportError as e:
+        raise ImproperlyConfigured("Could not find backend '%s': %s" % (
+            backend, e))
+
+    # Create backend
+    return backend_cls()
