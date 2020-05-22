6da1fb07d4d43177538e396973f997f8cc894f22
Eugen Ciur <eugen@django-lessons.com>
2020-05-22 08:32:41 +0200

    basic test for search backend

---
+++ papermerge/test/test_search.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+from papermerge.search.backends import get_search_backend
+
+User = get_user_model()
+
+class TestPage(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='test')
+
+    def test_search(self):
+        backend = get_search_backend()
+        backend.search("Great doc!")
