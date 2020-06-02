57c8f27cbacb675f7565a4d334eed09f0342c287
Eugen Ciur <eugen@django-lessons.com>
2020-06-02 11:00:28 +0200

    hello, kvstore

--- papermerge/core/models/kvstore.py
+++ papermerge/core/models/kvstore.py
@@ -5,10 +5,14 @@ class KVStore(models.Model):
     # e.g. shop, price, date
     key = models.CharField(
         max_length=200,
+        null=True,
+        blank=True
     )
     # e.g. groceries - full breadcrumb to the current node
     namespace = models.CharField(
-        max_length=1024
+        max_length=1024,
+        null=True,
+        blank=True
     )
     # (namespace, key) = primary key
     # e.g.
@@ -19,16 +23,20 @@ class KVStore(models.Model):
     #    namespaced_key = clients_retail_shop
     namespaced_key = models.CharField(
         max_length=200,
-        primary_key=True,
-        db_index=True,
-        unique=True
+        null=True,
+        blank=True
     )
 
-    value = models.TextField()
+    value = models.TextField(
+        null=True,
+        blank=True
+    )
 
     # human readable key
     human_key = models.CharField(
-        max_length=200
+        max_length=200,
+        null=True,
+        blank=True
     )
 
 
