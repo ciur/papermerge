614f3e2855a812adc3b8c09db2f341cd2be266a0
Eugen Ciur <eugen@papermerge.com>
2020-06-26 07:10:49 +0200

    new index page based on own admin contrib

--- papermerge/contrib/admin/views.py
+++ papermerge/contrib/admin/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 
 @login_required
-def browse(self):
-    return render("admin/browse.html")
+def browse(request):
+    return render(request, "admin/index.html")
