ace27a73b71c6af56439ff2c8accdb232b31fd84
Eugen Ciur <eugen@papermerge.com>
2020-06-30 11:56:29 +0200

    new UI

--- papermerge/contrib/admin/static/admin/js/papermerge.js
+++ papermerge/contrib/admin/static/admin/js/papermerge.js
@@ -19842,13 +19842,14 @@ class Browse extends backbone__WEBPACK_IMPORTED_MODULE_1__["Model"] {
 /*!*************************************!*\
   !*** ./src/js/models/dispatcher.js ***!
   \*************************************/
-/*! exports provided: mg_dispatcher, PARENT_CHANGED */
+/*! exports provided: mg_dispatcher, PARENT_CHANGED, SELECTION_CHANGED */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "mg_dispatcher", function() { return mg_dispatcher; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "PARENT_CHANGED", function() { return PARENT_CHANGED; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "SELECTION_CHANGED", function() { return SELECTION_CHANGED; });
 /* harmony import */ var underscore__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! underscore */ "./node_modules/underscore/modules/index-all.js");
 /* harmony import */ var backbone__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! backbone */ "./node_modules/backbone/backbone.js");
 /* harmony import */ var backbone__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(backbone__WEBPACK_IMPORTED_MODULE_1__);
@@ -19856,6 +19857,7 @@ __webpack_require__.r(__webpack_exports__);
 
 let mg_dispatcher = underscore__WEBPACK_IMPORTED_MODULE_0__["default"].clone(backbone__WEBPACK_IMPORTED_MODULE_1__["Events"]);
 let PARENT_CHANGED = "parent_changed";
+let SELECTION_CHANGED = "selection_changed";
 
 /***/ }),
 
@@ -21905,15 +21907,14 @@ class ActionsView extends backbone__WEBPACK_IMPORTED_MODULE_2__["View"] {
   }
 
   initialize(parent_id) {
-    let that = this;
-    this.parent_id = parent_id; // collection of nodes
+    this.parent_id = parent_id;
+    this.action_conditions = this._build_action_conditions(); // collection of nodes
 
     this.selection = new backbone__WEBPACK_IMPORTED_MODULE_2__["Collection"](); // collection of nodes
 
     this.clipboard = new backbone__WEBPACK_IMPORTED_MODULE_2__["Collection"]();
-    _models_dispatcher__WEBPACK_IMPORTED_MODULE_3__["mg_dispatcher"].on(_models_dispatcher__WEBPACK_IMPORTED_MODULE_3__["PARENT_CHANGED"], function (parent_id) {
-      that.parent_id = parent_id;
-    });
+    _models_dispatcher__WEBPACK_IMPORTED_MODULE_3__["mg_dispatcher"].on(_models_dispatcher__WEBPACK_IMPORTED_MODULE_3__["PARENT_CHANGED"], this.parent_changed, this);
+    _models_dispatcher__WEBPACK_IMPORTED_MODULE_3__["mg_dispatcher"].on(_models_dispatcher__WEBPACK_IMPORTED_MODULE_3__["SELECTION_CHANGED"], this.selection_changed, this);
   }
 
   events() {
@@ -21929,9 +21930,60 @@ class ActionsView extends backbone__WEBPACK_IMPORTED_MODULE_2__["View"] {
 
   rename_node(event) {}
 
+  parent_changed(parent_id) {
+    this.parent_id = parent_id;
+  }
+
+  selection_changed(selection) {
+    let that = this;
+    this.selection.reset(selection);
+
+    underscore__WEBPACK_IMPORTED_MODULE_1__["default"].each(this.action_conditions.models, function (item) {
+      if (item.get('cond')(that.selection, that.clipboard, that.parent_id)) {
+        that.enable_action(item);
+      } else {
+        that.disable_action(item);
+      }
+    });
+  }
+
   new_folder(event) {
     let new_folder_view;
     new_folder_view = new _views_new_folder__WEBPACK_IMPORTED_MODULE_4__["NewFolderView"](this.parent_id);
+  }
+
+  enable_action(item) {
+    jquery__WEBPACK_IMPORTED_MODULE_0___default()(item.get('id')).removeClass("disabled");
+  }
+
+  disable_action(item) {
+    jquery__WEBPACK_IMPORTED_MODULE_0___default()(item.get('id')).addClass("disabled");
+  }
+
+  _build_action_conditions() {
+    let that = this,
+        result = new backbone__WEBPACK_IMPORTED_MODULE_2__["Collection"]();
+    result.add({
+      'id': "#delete",
+      'cond': function (selection, clipboard, parent_id) {
+        if (selection.length > 0) {
+          return true;
+        }
+
+        return false;
+      }
+    });
+    result.add({
+      'id': "#rename",
+      'cond': function (selection, clipboard, parent_id) {
+        if (selection.length == 1) {
+          return true;
+        }
+
+        return false;
+      }
+    });
+    return result;
   }
 
 }
@@ -22084,7 +22136,15 @@ class BrowseView extends backbone__WEBPACK_IMPORTED_MODULE_3__["View"] {
       } else {
         $target.removeClass('checked');
       }
+
+      _models_dispatcher__WEBPACK_IMPORTED_MODULE_4__["mg_dispatcher"].trigger(_models_dispatcher__WEBPACK_IMPORTED_MODULE_4__["SELECTION_CHANGED"], this.get_selection());
     }
+  }
+
+  get_selection() {
+    return underscore__WEBPACK_IMPORTED_MODULE_1__["default"].filter(this.browse.nodes.models, function (item) {
+      return item.get('selected') == true;
+    });
   }
 
   open_node(event) {
