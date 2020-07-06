3b2748cdade782b55a33bdd14ddd9eca9ac51f01
Eugen Ciur <eugen@papermerge.com>
2020-07-06 13:39:45 +0200

    styling users and groups. Highlight active menu (based on url name)

--- papermerge/contrib/admin/templatetags/admin_tags.py
+++ papermerge/contrib/admin/templatetags/admin_tags.py
@@ -21,3 +21,24 @@ def ocr_language_select(user):
         languages.append(lang)
 
     return {'languages': languages}
+
+
+@register.simple_tag(takes_context=True)
+def activate_on(context, names):
+    """
+    names is a string of words separated by comma.
+    Example:
+        'group, groups'
+        'users, user'
+
+    Maybe a single word as well (without comma):
+        'user'
+    """
+    cleaned_names = [
+        name.strip() for name in names.split(',')
+    ]
+    if context['request'].resolver_match.url_name in cleaned_names:
+        return 'active'
+
+    return ''
+
