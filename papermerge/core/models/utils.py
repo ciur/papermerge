579077af45d1448a2b22ecb052e2ad851e99a23d
Eugen Ciur <eugen@papermerge.com>
2020-07-11 11:43:03 +0200

    Detect, catch, document and delete a very nasty BUG!

---
+++ papermerge/core/models/utils.py
@@ -0,0 +1,32 @@
+from papermerge.core.models import BaseTreeNode
+
+
+def recursive_delete(queryset_or_node_instance):
+    """
+    Given a queryset of nodes (BaseTreeNode instance),
+    traverse recursively (depth traversal) and delete
+    all nodes starting from leaf nodes upwards.
+
+    In theory this function is redundant as node.delete() should
+    take care of whole subtree.
+
+    But because of a nasty bug -
+        see papermerge.test.test_node.TestRecursiveDelete
+    it is a necessary workaround
+    """
+    if isinstance(queryset_or_node_instance, BaseTreeNode):
+        queryset = BaseTreeNode.objects.filter(
+            id=queryset_or_node_instance.id
+        )
+    else:
+        queryset = queryset_or_node_instance
+
+    for node in queryset:
+
+        descendants = node.get_descendants()
+        if descendants.count() > 0:
+            recursive_delete(descendants)
+
+        # At this point all descendants were deleted.
+        # Self delete :)
+        node.delete()
--- papermerge/test/test_node.py
+++ papermerge/test/test_node.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
-from papermerge.core.models import Document, Folder
+from papermerge.core.models import Document, Folder, BaseTreeNode
 from papermerge.test.utils import create_root_user
+from papermerge.core.models.utils import recursive_delete
 
 
 class TestNode(TestCase):
@@ -44,4 +45,134 @@ class TestNode(TestCase):
         )
         self.assertFalse(
             node.is_folder()
+        )
+
+
+class TestRecursiveDelete(TestCase):
+    """
+    Dedicated TestCase for very suble bug.
+
+    There 2 empty folders: A and B:
+                   Home
+                A         B
+
+    User uploads document to folder A:
+                   Home
+                A         B
+                |
+             document.pdf
+
+    User moves empty folder B (Cut -> Paste) into A:
+                 Home
+                   |
+                   A
+                 /   \
+                /     \
+        document.pdf   B
+
+    Expected: User can delete folder A (
+        recursively this must delete document.pdf and folder B)
+    Actual: When deleting folder A - there is a foreign key constrain error
+    """
+
+    def setUp(self):
+        self.user = create_root_user()
+
+    def test_delete_folder_with_document(self):
+
+        folder_A = Folder.objects.create(
+            title="A",
+            user=self.user
+        )
+
+        folder_B = Folder.objects.create(
+            title="B",
+            user=self.user,
+        )
+
+        doc = Document.create_document(
+            title="document.pdf",
+            file_name="document.pdf",
+            size='1212',
+            lang='DEU',
+            user=self.user,
+            parent_id=folder_A.id,
+            page_count=5,
+        )
+        doc.save()
+
+        BaseTreeNode.objects.move_node(folder_B, folder_A)
+
+        folder_A.refresh_from_db()
+        # at this point, folder_A will have 2 descendants:
+        #   * folder B
+        #   * document.pdf
+        descendants_count = folder_A.get_descendants(
+            include_self=False
+        ).count()
+
+        self.assertEqual(
+            2,
+            descendants_count
+        )
+        recursive_delete(folder_A)
+
+        # by now everything should be deleted
+        self.assertEqual(
+            0,
+            BaseTreeNode.objects.count()
+        )
+
+    def test_delete_folder_with_recentely_moved_in_descendant(self):
+        """
+        Related to test_delete_folder_with_document.
+        This test assert that user can delete folders with recently
+        moved in folders.
+        There 2 empty folders: A and B:
+                       Home
+                    A         B
+
+        User moves empty folder B (Cut -> Paste) into A:
+                     Home
+                       |
+                       A
+                       |
+                       |
+                       B
+
+        Expected: User can delete folder A (
+            recursively this must delete folder B)
+
+        """
+        folder_A = Folder.objects.create(
+            title="A",
+            user=self.user
+        )
+
+        folder_B = Folder.objects.create(
+            title="B",
+            user=self.user,
+        )
+
+        Folder.objects.move_node(folder_B, folder_A)
+
+        folder_A.refresh_from_db()
+        # at this point, folder_A will have 2 descendants:
+        #   * folder B
+        #   * document.pdf
+        descendants_count = folder_A.get_descendants(
+            include_self=False
+        ).count()
+
+        self.assertEqual(
+            1,
+            descendants_count
+        )
+
+        # no exception should be raised here.
+        folder_A.delete()
+        # no folders left
+        self.assertEqual(
+            0,
+            Folder.objects.count()
         )
