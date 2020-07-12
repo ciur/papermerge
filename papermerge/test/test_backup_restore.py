b8c4d9943d8f892cad54c69b33d74fa8bdf096a8
frenos <frenos@users.noreply.github.com>
2020-07-12 15:33:21 +0200

    add more testing to backup

--- papermerge/test/test_backup_restore.py
+++ papermerge/test/test_backup_restore.py
@@ -6,7 +6,7 @@ import tarfile
 
 from django.test import TestCase
 
-from papermerge.core.models import Document
+from papermerge.core.models import Document, Folder
 from papermerge.core.storage import default_storage
 from papermerge.test.utils import create_root_user
 from papermerge.core.backup_restore import backup_documents, _can_restore
@@ -19,8 +19,7 @@ class TestBackupRestore(TestCase):
     def setUp(self) -> None:
         self.testcase_user = create_root_user()
 
-    def test_single_document(self):
-
+    def test_backup_single_document(self):
         document_path = os.path.join(
             BASE_DIR, "data", "berlin.pdf"
         )
@@ -39,9 +38,62 @@ class TestBackupRestore(TestCase):
             dst=doc.path.url(),
         )
 
-        # Check if document is created
-        new_document = Document.objects.filter(title='berlin.pdf').first()
-        self.assertIsNotNone(new_document)
+        with io.BytesIO() as memoryfile:
+            backup_documents(memoryfile)
+            memoryfile.seek(0)
+
+            self.assertTrue(_can_restore(memoryfile), 'generated backup.tar is not valid')
+            memoryfile.seek(0)
+
+            backup_file = tarfile.open(fileobj=memoryfile, mode='r')
+            backup_json = backup_file.extractfile('backup.json')
+            backup_info = json.loads(backup_json.read())
+
+            self.assertIsNotNone(backup_info.get('documents'), 'backup.json did not have a key "documents"')
+            self.assertIs(len(backup_info.get('documents')), 1,
+                          'backup.json key documents had more or less than one entry')
+            self.assertIs(len(backup_file.getnames()), 2, 'backup.tar had more or less than 2 entries')
+            self.assertTrue('berlin.pdf' in backup_file.getnames(), 'berlin.pdf was not in the backup.tar')
+
+    def test_backup_document_hierachy(self):
+        folder_1 = Folder.objects.create(title='1', parent=None, user=self.testcase_user)
+        folder_2 = Folder.objects.create(title='2', parent=folder_1, user=self.testcase_user)
+        folder_3 = Folder.objects.create(title='3', parent=folder_1, user=self.testcase_user)
+        folder_4 = Folder.objects.create(title='4', parent=None, user=self.testcase_user)
+
+        document_path = os.path.join(
+            BASE_DIR, "data", "berlin.pdf"
+        )
+
+        doc_1 = Document.create_document(
+            user=self.testcase_user,
+            title='berlin.pdf',
+            size=os.path.getsize(document_path),
+            lang='deu',
+            file_name='berlin.pdf',
+            parent_id=folder_2.id,
+            page_count=3
+        )
+
+        default_storage.copy_doc(
+            src=document_path,
+            dst=doc_1.path.url(),
+        )
+
+        doc_2 = Document.create_document(
+            user=self.testcase_user,
+            title='berlin.pdf',
+            size=os.path.getsize(document_path),
+            lang='deu',
+            file_name='berlin.pdf',
+            parent_id=folder_3.id,
+            page_count=3
+        )
+
+        default_storage.copy_doc(
+            src=document_path,
+            dst=doc_2.path.url(),
+        )
 
         with io.BytesIO() as memoryfile:
             backup_documents(memoryfile)
@@ -55,10 +107,12 @@ class TestBackupRestore(TestCase):
             backup_info = json.loads(backup_json.read())
 
             self.assertIsNotNone(backup_info.get('documents'), 'backup.json did not have a key "documents"')
-            self.assertIs(len(backup_info.get('documents')),1, 'backup.json key documents had more or less than one entry')
-            self.assertIs(len(backup_file.getnames()), 2, 'backup.tar had more or less than 2 entries')
-            self.assertTrue('berlin.pdf' in backup_file.getnames(), 'berlin.pdf was not in the backup.tar')
-
-
-
-
+            self.assertIs(len(backup_info.get('documents')), 2,
+                          'backup.json key documents had more or less than two entry')
+            self.assertIs(len(backup_file.getnames()), 3, 'backup.tar had more or less than 2 entries')
+            self.assertTrue('1/2/berlin.pdf' in backup_file.getnames(),
+                            'berlin.pdf was not in the backup.tar at folder 1/2/')
+            self.assertTrue('1/3/berlin.pdf' in backup_file.getnames(),
+                            'berlin.pdf was not in the backup.tar at folder 1/3/')
+            self.assertFalse('4' in backup_file.getnames(),
+                             'Folder 4 was in backup.tar but should have been ignored')
