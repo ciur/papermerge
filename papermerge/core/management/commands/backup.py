ab2c2a7609dbdd823aa5a772b449b79579039ad1
Eugen Ciur <eugen@papermerge.com>
2020-07-13 09:14:53 +0200

    In backup command consider location = directory case

--- papermerge/core/management/commands/backup.py
+++ papermerge/core/management/commands/backup.py
@@ -1,23 +1,47 @@
+import logging
 import datetime
+import os
 
 from django.core.management import BaseCommand
 from papermerge.core.backup_restore import backup_documents
 
+logger = logging.getLogger(__name__)
+
 
 class Command(BaseCommand):
     help = """
-        Backup all Documents and their folder structure to a archive.
-         
+        Backup all documents and their folder structure to a archive.
     """
 
     def add_arguments(self, parser):
-        parser.add_argument('location',nargs='?', type=str)
+        parser.add_argument(
+            'location',
+            nargs='?',
+            type=str,
+            help="A directory or a file name (tar archive)."
+            " If provided file is missing - will create one."
+        )
 
     def handle(self, *args, **options):
-        if options.get('location') is None:
-            date_string = datetime.datetime.now().strftime("%d_%m_%Y-%H_%M_%S")
-            backup_location = 'backup_%s.tar'%(date_string)
+
+        date_string = datetime.datetime.now().strftime("%d_%m_%Y-%H_%M_%S")
+        default_filename = f"backup_{date_string}.tar"
+
+        location = options.get('location') or default_filename
+
+        # consider the case when user provides directory location i.e.
+        # ./manage.py backup /backup/papermerge/
+
+        if os.path.isdir(location):
+            backup_location = os.path.join(location, default_filename)
         else:
-            backup_location = options.get('location')
-        with open(backup_location, 'wb') as backup_file:
-            backup_documents(backup_file)
+            backup_location = location
+
+        try:
+            with open(backup_location, 'wb') as backup_file:
+                backup_documents(backup_file)
+        except IsADirectoryError:
+            logger.error(
+                "Provided location is a directory which does not exist."
+                "Please create it first and try again."
+            )
