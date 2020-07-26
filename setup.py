1dded76fe526a00ac2c7a6be8f61e2d777dbdb18
Eugen Ciur <eugen@papermerge.com>
2020-07-26 09:07:04 +0200

    lower python requirements to 3.7

--- setup.py
+++ setup.py
@@ -26,5 +26,5 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.7',
 )
