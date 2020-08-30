16d34a93c09a17acc61e646ae565cce5d03643a6
Eugen Ciur <eugen@papermerge.com>
2020-08-30 12:10:03 +0200

    human friendly UI logs

--- papermerge/contrib/admin/signals.py
+++ papermerge/contrib/admin/signals.py
@@ -1,7 +1,9 @@
+import logging
+
 from django.dispatch import receiver
+from django.utils.translation import gettext as _
 
-
-from papermerge.core.models import Folder
+from papermerge.core.models import Folder, Document
 
 from papermerge.core.signal_definitions import (
     folder_created,
@@ -10,11 +12,62 @@ from papermerge.core.signal_definitions import (
 )
 from papermerge.core.utils import node_tag
 from papermerge.contrib.admin.models import LogEntry
+from papermerge.core.ocr import (
+    COMPLETE,
+    STARTED
+)
 
 
 @receiver(page_ocr)
 def page_ocr_handler(sender, **kwargs):
-    pass
+    user_id = kwargs.get('user_id')
+    level = kwargs.get('level')
+    doc_id = kwargs.get('document_id')
+    message = kwargs.get('message')
+    page_num = kwargs.get('page_num')
+    lang = kwargs.get('lang')
+    status = kwargs.get('status')
+
+    if status == COMPLETE:
+        human_status = _("complete")
+    else:
+        human_status = _("started")
+
+    try:
+        doc = Document.object.get(id=doc_id)
+    except Document.DoesNotExist:
+        LogEntry.objects.create(
+            user_id=user_id,
+            level=logging.WARGNING,
+            message=_(
+                f"OCR {human_status} for doc_id={doc_id}, page {page_num}."
+                f"But in meantime document probably was deleted"
+            )
+        )
+
+        return
+
+    doc_tag = node_tag(doc)
+
+    log_entry_message = ""
+
+    if status == STARTED:
+        log_entry_message = _(
+            f"OCR {human_status} for document {doc_tag}, page {page_num}."
+            f" language={lang} "
+        )
+    elif status == COMPLETE:
+        log_entry_message = _(
+            f"OCR {human_status} for document {doc_tag}, page {page_num}."
+            f" language={lang}. "
+        )
+        log_entry_message += message
+
+    LogEntry.objects.create(
+        user_id=user_id,
+        level=level,
+        message=log_entry_message
+    )
 
 
 @receiver(folder_created)
@@ -27,7 +80,7 @@ def folder_created_handler(sender, **kwargs):
 
     folder_tag = node_tag(folder)
 
-    msg = f"Node/Folder {folder_tag} created"
+    msg = f"Node/Folder {folder_tag} created."
 
     LogEntry.objects.create(
         user_id=user_id,
@@ -42,7 +95,7 @@ def nodes_deleted_handler(sender, **kwargs):
     user_id = kwargs.get('user_id')
     level = kwargs.get('level')
 
-    msg = f"Node(s) {','.join(node_tags)} were deleted"
+    msg = f"Node(s) {','.join(node_tags)} were deleted."
 
     LogEntry.objects.create(
         user_id=user_id,
