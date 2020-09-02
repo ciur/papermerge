0967ae2654a6ec47c1f29d0ab7362fe9a1601836
Eugen Ciur <eugen@papermerge.com>
2020-09-02 08:15:25 +0200

    dev.py, commented logging settings

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -2,37 +2,26 @@ from .base import *  # noqa
 
 DEBUG = True
 
+# INSTALLED_APPS.extend(
+#     ['django_extensions']
+# )
+
 INTERNAL_IPS = ['127.0.0.1', ]
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-        'file': {
-            'class': 'logging.FileHandler',
-            'filename': 'ocr.log',
-            'level': 'DEBUG'
-        },
-    },
-    'loggers': {
-        'papermerge': {
-            'handlers': ['console'],
-            'level': 'DEBUG'
-        },
-        'celery': {
-            'handlers': ['console'],
-            'level': 'INFO'
-        },
-        'papermerge.core.ocr': {
-            'handlers': ['file'],
-            'level': 'DEBUG'
-        },
-        'mglib': {
-            'handlers': ['file'],
-            'level': 'DEBUG'
-        },
-    },
-}
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'papermerge': {
+#             'class': 'logging.FileHandler',
+#             'filename': 'papermerge.log',
+#             'level': 'DEBUG'
+#         },
+#     },
+#     'loggers': {
+#         'papermerge.core.importers': {
+#             'handlers': ['papermerge'],
+#             'level': 'DEBUG'
+#         },
+#     },
+# }
