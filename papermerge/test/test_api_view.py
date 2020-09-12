b4b01c8e4be197d0f4e1c53ecb2fbd2d7c2f92f3
Eugen Ciur <eugen@papermerge.com>
2020-09-12 17:29:31 +0200

    update rest api with user perms. Add UT

--- papermerge/test/test_api_view.py
+++ papermerge/test/test_api_view.py
@@ -8,16 +8,12 @@ from django.urls import reverse
 from django.http.response import (
     HttpResponseForbidden
 )
-from django.test.client import (
-    RequestFactory,
-    encode_multipart
-)
 
 from rest_framework.test import APIClient
+from rest_framework import status
 from knox.models import AuthToken
 
 from papermerge.core.models import Document
-from papermerge.core.views.api import DocumentUploadView
 
 
 from .utils import (
@@ -139,6 +135,7 @@ class TestRestApiWithValidToken(TestCase):
     def setUp(self):
 
         self.testcase_user = create_root_user()
+        self.margaret_user = create_margaret_user()
         self.token = self._create_token(
             self.testcase_user
         )
@@ -185,4 +182,271 @@ class TestRestApiWithValidToken(TestCase):
         self.assertEquals(
             Document.objects.count(),
             1
+        )
+
+    def test_basic_documents_view(self):
+        # create a basic document and assert
+        # that api/documents returns it
+        Document.create_document(
+            title="berlin.pdf",
+            user=self.testcase_user,
+            lang="ENG",
+            file_name="berlin.pdf",
+            size=1222,
+            page_count=3
+        )
+        ret = self.client.get(
+            reverse('core:api_documents')
+        )
+
+        self.assertEqual(
+            ret.status_code, 200
+        )
+        docs = json.loads(ret.content)
+        self.assertEqual(
+            len(docs),
+            1
+        )
+
+    def test_documents_returns_only_docs_user_has_perms_for_1(self):
+        """
+        User can list via API only docs he/she has access READ_ACCESS
+        """
+        # create a basic document and assert
+        # that api/documents returns it
+        Document.create_document(
+            title="berlin.pdf",
+            user=self.margaret_user,
+            lang="ENG",
+            file_name="berlin.pdf",
+            size=1222,
+            page_count=3
+        )
+        ret = self.client.get(
+            reverse('core:api_documents')
+        )
+
+        self.assertEqual(
+            ret.status_code, 200
+        )
+        docs = json.loads(ret.content)
+        # There is only one document in the system, and
+        # only margaret has access to it (as owner)
+        self.assertEqual(
+            len(docs),
+            0
+        )
+
+    def test_documents_returns_only_docs_user_has_perms_for_2(self):
+        """
+        User can list via API only docs he/she has access READ_ACCESS
+        """
+        # create a basic document and assert
+        # that api/documents returns it
+        Document.create_document(
+            title="berlin.pdf",
+            user=self.margaret_user,
+            lang="ENG",
+            file_name="berlin.pdf",
+            size=1222,
+            page_count=3
+        )
+        Document.create_document(
+            title="doc1.pdf",
+            user=self.testcase_user,
+            lang="ENG",
+            file_name="berlin.pdf",
+            size=1222,
+            page_count=3
+        )
+        Document.create_document(
+            title="doc2.pdf",
+            user=self.testcase_user,
+            lang="ENG",
+            file_name="berlin.pdf",
+            size=1222,
+            page_count=3
+        )
+
+        ret = self.client.get(
+            reverse('core:api_documents')
+        )
+
+        self.assertEqual(
+            ret.status_code, 200
+        )
+        docs = json.loads(ret.content)
+        # There is only one document in the system, and
+        # only margaret has access to it (as owner)
+        self.assertEqual(
+            len(docs),
+            2  # returns a list contaiing doc1.pdf and doc2.pdf
+        )
+        returned_titles = list(
+            map(
+                lambda x: x['title'],
+                docs
+            )
+        )
+        self.assertEquals(
+            set(["doc1.pdf", "doc2.pdf"]),
+            set(returned_titles)
+        )
+
+    def test_returns_only_doc_user_has_perms_for_1(self):
+        """
+        User can view via API only document he/she has access READ_ACCESS
+        """
+        doc = Document.create_document(
+            title="berlin.pdf",
+            user=self.margaret_user,
+            lang="ENG",
+            file_name="berlin.pdf",
+            size=1222,
+            page_count=3
+        )
+        ret = self.client.get(
+            reverse('core:api_document', args=(doc.id,))
+        )
+
+        self.assertEqual(
+            ret.status_code,
+            HttpResponseForbidden.status_code
+        )
+
+    def test_returns_only_doc_user_has_perms_for_2(self):
+        """
+        User can view via API only document he/she has access READ_ACCESS
+        """
+        doc = Document.create_document(
+            title="berlin.pdf",
+            user=self.testcase_user,
+            lang="ENG",
+            file_name="berlin.pdf",
+            size=1222,
+            page_count=3
+        )
+        ret = self.client.get(
+            reverse('core:api_document', args=(doc.id,))
+        )
+
+        self.assertEqual(
+            ret.status_code,
+            200
+        )
+
+        doc = json.loads(ret.content)
+        self.assertEqual(
+            doc['title'],
+            "berlin.pdf"
+        )
+
+    def test_delete_denied_if_no_perm_granted(self):
+        # document owned by margaret
+        doc = Document.create_document(
+            title="berlin.pdf",
+            user=self.margaret_user,
+            lang="ENG",
+            file_name="berlin.pdf",
+            size=1222,
+            page_count=3
+        )
+        # access is denied for testcase_user
+        ret = self.client.delete(
+            reverse('core:api_document', args=(doc.id,))
+        )
+
+        self.assertEqual(
+            ret.status_code,
+            HttpResponseForbidden.status_code
+        )
+
+        self.assertEqual(
+            Document.objects.count(),
+            1
+        )
+
+    def test_edit_denied_if_no_perm_granted(self):
+        # document owned by margaret
+        doc = Document.create_document(
+            title="berlin.pdf",
+            user=self.margaret_user,
+            lang="ENG",
+            file_name="berlin.pdf",
+            size=1222,
+            page_count=3
+        )
+        # access is denied for testcase_user
+        ret = self.client.put(
+            reverse('core:api_document', args=(doc.id,)),
+            {'title': "OK"},
+            format="json"
+        )
+
+        self.assertEqual(
+            ret.status_code,
+            HttpResponseForbidden.status_code
+        )
+
+        self.assertEqual(
+            Document.objects.count(),
+            1
+        )
+
+    def test_can_edit_own_doc(self):
+        # document owned by margaret
+        doc = Document.create_document(
+            title="berlin.pdf",
+            user=self.testcase_user,
+            lang="ENG",
+            file_name="berlin.pdf",
+            size=1222,
+            page_count=3
+        )
+        # access is denied for testcase_user
+        ret = self.client.put(
+            reverse('core:api_document', args=(doc.id,)),
+            {'title': "OK.pdf"},
+            format="json"
+        )
+
+        self.assertEqual(
+            ret.status_code,
+            200
+        )
+
+        doc_after_edit = json.loads(ret.content)
+
+        self.assertEqual(doc_after_edit['id'], doc.id)
+        self.assertEqual(doc_after_edit['title'], "OK.pdf")
+
+
+class TestRestApiValidToken_With_InvalidToken(TestCase):
+    def setUp(self):
+
+        self.testcase_user = create_root_user()
+        self.token = "Invalid Token"
+        self.client = APIClient(
+            HTTP_AUTHORIZATION=f"Token {self.token}",
+        )
+
+    def test_basic_documents_view(self):
+        # create a basic document and assert
+        # that api/documents returns it
+        Document.create_document(
+            title="berlin.pdf",
+            user=self.testcase_user,
+            lang="ENG",
+            file_name="berlin.pdf",
+            size=1222,
+            page_count=3
+        )
+        ret = self.client.get(
+            reverse('core:api_documents')
+        )
+
+        # because user's token is invalid
+        self.assertEqual(
+            ret.status_code,
+            status.HTTP_401_UNAUTHORIZED
         )
