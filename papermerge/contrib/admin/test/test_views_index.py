a021546363600cb8469c4e47997b8742b21468c9
Eugen Ciur <eugen@papermerge.com>
2020-09-16 06:46:50 +0200

    add UT for advanced search - search by tags

--- papermerge/contrib/admin/test/test_views_index.py
+++ papermerge/contrib/admin/test/test_views_index.py
@@ -23,9 +23,6 @@ class TestAdvancedSearchView(TestCase):
     def test_basic_av_by_tag(self):
         """
         In advaced search user can search by tag(s)
-        tags_op can be 'all' or 'any'.
-        tags_op=all: find all documents which contain all tags
-        tags_op=any: find all documents which contain at least one of tags
         """
         doc1 = Document.create_document(
             title="doc1",
@@ -54,11 +51,7 @@ class TestAdvancedSearchView(TestCase):
         )
 
         ret = self.client.get(
-            reverse('admin:search'),
-            {
-                'tag': 'green',
-                'tags_op': 'all'
-            }
+            reverse('admin:search'), {'tag': 'green'}
         )
         self.assertEquals(
             ret.status_code,
@@ -69,4 +62,142 @@ class TestAdvancedSearchView(TestCase):
             1
         )
         doc_ = ret.context['results_docs'][0]
-        self.assertEquals(doc_.id, doc1.id)
+
+        self.assertEquals(
+            doc_.id,
+            doc1.id
+        )
+
+    def test_basic_av_by_tags_op_all(self):
+        """
+        In advaced search user can search by tag(s)
+        tags_op can be 'all' or 'any'.
+        tags_op=all: find all documents which contain all tags
+        """
+        doc1 = Document.create_document(
+            title="doc1",
+            user=self.testcase_user,
+            page_count=2,
+            file_name="koko.pdf",
+            size='1111',
+            lang='ENG',
+        )
+        doc2 = Document.create_document(
+            title="doc2",
+            user=self.testcase_user,
+            page_count=2,
+            file_name="kuku.pdf",
+            size='1111',
+            lang='ENG',
+        )
+        doc3 = Document.create_document(
+            title="doc3",
+            user=self.testcase_user,
+            page_count=2,
+            file_name="momo.pdf",
+            size='1111',
+            lang='ENG',
+        )
+        doc1.tags.add(
+            "green",
+            "blue",
+            tag_kwargs={'user': self.testcase_user}
+        )
+        doc2.tags.add(
+            "blue",
+            tag_kwargs={'user': self.testcase_user}
+        )
+        doc3.tags.add(
+            "green",
+            "blue",
+            "red",
+            tag_kwargs={'user': self.testcase_user}
+        )
+
+        base_url = reverse('admin:search')
+        args = "tag=green&tag=blue&tags_op=all"
+        url = f"{base_url}?{args}"
+
+        ret = self.client.get(url)
+
+        self.assertEquals(
+            ret.status_code,
+            200
+        )
+        self.assertEquals(
+            len(ret.context['results_docs']),
+            2
+        )
+        result_ids = set(
+            [doc_.id for doc_ in ret.context['results_docs']]
+        )
+        self.assertEquals(
+            result_ids,
+            set([doc1.id, doc3.id])
+        )
+
+    def test_basic_av_by_tags_op_any(self):
+        """
+        In advaced search user can search by tag(s)
+        tags_op can be 'all' or 'any'.
+        tags_op=any: find all documents which contain any tags
+        of the mentioned tags
+        """
+        doc1 = Document.create_document(
+            title="doc1",
+            user=self.testcase_user,
+            page_count=2,
+            file_name="koko.pdf",
+            size='1111',
+            lang='ENG',
+        )
+        doc2 = Document.create_document(
+            title="doc2",
+            user=self.testcase_user,
+            page_count=2,
+            file_name="kuku.pdf",
+            size='1111',
+            lang='ENG',
+        )
+        doc3 = Document.create_document(
+            title="doc3",
+            user=self.testcase_user,
+            page_count=2,
+            file_name="momo.pdf",
+            size='1111',
+            lang='ENG',
+        )
+        doc1.tags.add(
+            "red",
+            tag_kwargs={'user': self.testcase_user}
+        )
+        doc2.tags.add(
+            "green",
+            tag_kwargs={'user': self.testcase_user}
+        )
+        doc3.tags.add(
+            "blue",
+            tag_kwargs={'user': self.testcase_user}
+        )
+
+        base_url = reverse('admin:search')
+        args = "tag=red&tag=green&tags_op=any"
+        url = f"{base_url}?{args}"
+
+        ret = self.client.get(url)
+
+        self.assertEquals(
+            ret.status_code,
+            200
+        )
+        self.assertEquals(
+            len(ret.context['results_docs']),
+            2
+        )
+        result_ids = set(
+            [doc_.id for doc_ in ret.context['results_docs']]
+        )
+        self.assertEquals(
+            result_ids,
+            set([doc1.id, doc2.id])
+        )
