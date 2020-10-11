29ec5a88357f25d7b92b0618f634f20c326535d2
Eugen Ciur <eugen@papermerge.com>
2020-10-11 12:56:56 +0200

    Fixes broker startup script. Issue #153.

--- docker/1.4/app.startup.sh
+++ docker/1.4/app.startup.sh
@@ -12,10 +12,13 @@ cat create_user.py | python3 manage.py shell
 ./manage.py check
 
 mod_wsgi-express start-server \
-    --server-root /opt/app/ \
+    --server-root /opt/app/\
     --url-alias /static /opt/static  \
     --url-alias /media /opt/media \
     --port 8000 --user www --group www \
     --log-to-terminal \
-    --limit-request-body 20971520 \  # max upload size = 20 MB
+    --limit-request-body 20971520 \
     config/wsgi.py
+
+# --limit-request-body 20971520 options is to limit
+# max upload size to 20 MB
