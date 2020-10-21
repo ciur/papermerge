254344dd1f8fd61a25b97d0ac6ff7f350a1ba4e5
Eugen Ciur <eugen@papermerge.com>
2020-10-21 11:13:08 +0200

    preferences refactoring

--- papermerge/contrib/admin/views/preferences.py
+++ papermerge/contrib/admin/views/preferences.py
@@ -3,6 +3,7 @@ import logging
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from dynamic_preferences.users.forms import user_preference_form_builder
+from dynamic_preferences.settings import preferences_settings
 
 
 logger = logging.getLogger(__name__)
@@ -20,13 +21,14 @@ def preferences_section_view(request, section):
         form = Form(request.POST)
         if form.is_valid():
             form.update_preferences()
-            return redirect('admin:index')
+            return redirect('admin:preferences')
 
     return render(
         request,
         'admin/preferences_section.html',
         {
             'form': Form,
+            'section': section
         }
     )
 
@@ -34,18 +36,14 @@ def preferences_section_view(request, section):
 @login_required
 def preferences_view(request):
 
-    Form = user_preference_form_builder(instance=request.user)
-
-    if request.method == 'POST':
-        form = Form(request.POST)
-        if form.is_valid():
-            form.update_preferences()
-            return redirect('admin:index')
-
+    sections = [
+        val.split(preferences_settings.SECTION_KEY_SEPARATOR)[0]
+        for val in request.user.preferences.all()
+    ]
     return render(
         request,
         'admin/preferences.html',
         {
-            'form': Form,
+            'sections': sections,
         }
     )
