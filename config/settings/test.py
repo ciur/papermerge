50b8d8bbb46835162eddfdb2760ee3ab9981e16a
Eugen Ciur <eugen@papermerge.com>
2020-10-29 08:36:07 +0100

    speed up test suite execution (by 20%)

--- config/settings/test.py
+++ config/settings/test.py
@@ -63,6 +63,14 @@ REST_KNOX = {
 
 CELERY_BROKER_URL = "memory://"
 
+# The default password hasher is rather slow by design.
+# If you’re authenticating many users in your tests,
+# you may want to use a custom settings file and set
+# the PASSWORD_HASHERS setting to a faster hashing algorithm:
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
+
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
@@ -81,10 +89,6 @@ LOGGING = {
         'level': 'CRITICAL'
     },
 }
-
-PAPERMERGE_METADATA_PLUGINS = [
-    'papermerge.test.metaplugin.Dummy',
-]
 
 MEDIA_ROOT = os.path.join(
     PROJ_ROOT,
