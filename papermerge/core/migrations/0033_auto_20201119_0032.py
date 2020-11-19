63dfe6192083d26873bb2d16e0f3027f442e5df1
francesco.carzaniga <francesco.carzaniga@gmail.com>
2020-11-19 01:16:50 +0100

    Fix migrations

--- papermerge/core/migrations/0033_auto_20201119_0032.py
+++ papermerge/core/migrations/0033_auto_20201119_0032.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0032_auto_20201118_1523'),
+        ('core', '0032_auto_20201118_1628'),
     ]
 
     operations = [
