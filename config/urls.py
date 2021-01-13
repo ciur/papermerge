b6ca8518587f6f53bf022d7d2d06759ae12d2242
Eugen Ciur <eugen@papermerge.com>
2021-01-13 15:33:25 +0100

    add django debug toolbar as dev tool

--- config/urls.py
+++ config/urls.py
@@ -107,3 +107,12 @@ if settings.DEBUG:
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT
     )
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns.extend([
+        path(
+            '__debug__/', include(debug_toolbar.urls)
+        )
+    ])
+
