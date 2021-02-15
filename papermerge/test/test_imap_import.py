64cd8ac12bb28d49a9c1f17266659bcc6ef009be
Eugen Ciur <eugen@papermerge.com>
2021-02-15 10:41:04 +0100

    fix unit tests

--- papermerge/test/test_imap_import.py
+++ papermerge/test/test_imap_import.py
@@ -2,7 +2,7 @@ import os
 from email.message import EmailMessage
 import email
 
-from django.test import TestCase, override_settings
+from django.test import TestCase
 
 from .utils import (
     create_root_user,
@@ -10,13 +10,203 @@ from .utils import (
     create_elizabet_user
 )
 
-from papermerge.core.importers.imap import extract_info_from_email, read_email_message
+from papermerge.core.importers.imap import (
+    get_matching_user,
+    trigger_document_pipeline
+)
 from papermerge.core.models import Document
 
 BASE_DIR = os.path.dirname(__file__)
 
 
-def create_email(
+class TestIMAPImporterByUser(TestCase):
+
+    def setUp(self):
+        self.user = create_root_user()
+        self.margaret = create_margaret_user()
+        self.elizabet = create_elizabet_user()
+
+    def test_extract_user_by_mail_address_not_allowed_user(self):
+
+        body = _create_email(user=self.margaret)
+        email_message = email.message_from_bytes(
+            body,
+            policy=email.policy.default
+        )
+
+        user = get_matching_user(
+            email_message,
+            by_user=True
+        )
+
+        self.assertIsNone(user)
+
+    def test_extract_user_by_mail_address_allowed_user(self):
+
+        body = _create_email(user=self.elizabet)
+        email_message = email.message_from_bytes(
+            body, policy=email.policy.default)
+        self.elizabet.mail_by_user = True
+        self.elizabet.save()
+
+        user = get_matching_user(
+            email_message,
+            by_user=True
+        )
+
+        self.assertNotEqual(user, self.margaret)
+        self.assertEqual(user, self.elizabet)
+
+    def test_extract_user_by_mail_address_not_allowed_settings(self):
+        body = _create_email(user=self.margaret)
+        email_message = email.message_from_bytes(
+            body, policy=email.policy.default)
+        user = get_matching_user(email_message)
+        self.assertIsNone(user)
+
+    def test_extract_user_by_mail_address_allowed_settings(self):
+        body = _create_email(user=self.elizabet)
+        email_message = email.message_from_bytes(
+            body, policy=email.policy.default)
+        self.elizabet.mail_by_user = True
+        self.elizabet.save()
+        user = get_matching_user(email_message)
+        self.assertIsNone(user)
+
+
+class TestIMAPImporterBySecret(TestCase):
+
+    def setUp(self):
+
+        self.user = create_root_user()
+        self.margaret = create_margaret_user()
+        self.elizabet = create_elizabet_user()
+        self.margaret.mail_secret = 'this-is-margaret-secret'
+        self.margaret.save()
+        self.elizabet.mail_secret = 'this-is-elizabet-secret'
+        self.elizabet.save()
+
+    def test_extract_user_by_secret_not_allowed_user(self):
+
+        body = _create_email(subject='SECRET{this-is-margaret-secret}')
+        email_message = email.message_from_bytes(
+            body, policy=email.policy.default)
+
+        user = get_matching_user(
+            email_message,
+            by_secret=True
+        )
+
+        self.assertIsNone(user)
+
+    def test_extract_user_by_secret_allowed_user(self):
+
+        body = _create_email(subject='SECRET{this-is-elizabet-secret}')
+        email_message = email.message_from_bytes(
+            body, policy=email.policy.default)
+        self.elizabet.mail_by_secret = True
+        self.elizabet.save()
+
+        user = get_matching_user(
+            email_message,
+            by_secret=True
+        )
+
+        self.assertNotEqual(user, self.margaret)
+        self.assertEqual(user, self.elizabet)
+
+    def test_extract_user_by_secret_not_allowed_settings(self):
+
+        body = _create_email(subject='SECRET{this-is-margaret-secret}')
+        email_message = email.message_from_bytes(
+            body, policy=email.policy.default)
+        user = get_matching_user(email_message)
+        self.assertIsNone(user, self.margaret)
+
+    def test_extract_user_by_secret_allowed_settings(self):
+
+        body = _create_email(subject='SECRET{this-is-elizabet-secret}')
+        email_message = email.message_from_bytes(
+            body, policy=email.policy.default)
+        self.elizabet.mail_by_secret = True
+        self.elizabet.save()
+        user = get_matching_user(email_message)
+        self.assertIsNone(user)
+
+
+class TestIMAPImporterIngestion(TestCase):
+
+    def setUp(self):
+        self.user = create_root_user()
+
+    def test_pdf_imap_importer(self):
+        file_path = os.path.join(
+            BASE_DIR,
+            "data",
+            "berlin.pdf"
+        )
+        body = _create_email(
+            attachment=file_path,
+            maintype='application',
+            subtype='pdf',
+        )
+        email_message = email.message_from_bytes(
+            body, policy=email.policy.default)
+        user = get_matching_user(email_message)
+        self.assertIsNone(user)
+        trigger_document_pipeline(email_message, user, skip_ocr=True)
+        self.assertEqual(
+            Document.objects.count(),
+            1
+        )
+
+    def test_jpg_imap_importer(self):
+
+        file_path = os.path.join(
+            BASE_DIR,
+            "data",
+            "page-1.jpg"
+        )
+        body = _create_email(
+            attachment=file_path,
+            maintype='image',
+            subtype='jpg',
+        )
+        email_message = email.message_from_bytes(
+            body, policy=email.policy.default)
+        user = get_matching_user(email_message)
+        self.assertIsNone(user)
+        trigger_document_pipeline(email_message, user, skip_ocr=True)
+        self.assertEqual(
+            Document.objects.count(),
+            1
+        )
+
+    def test_tar_imap_importer(self):
+
+        file_path = os.path.join(
+            BASE_DIR,
+            "data",
+            "testdata.tar"
+        )
+        body = _create_email(
+            attachment=file_path,
+            maintype='application',
+            subtype='x-tar',
+        )
+        email_message = email.message_from_bytes(
+            body, policy=email.policy.default)
+        user = get_matching_user(email_message)
+        self.assertIsNone(user)
+        trigger_document_pipeline(email_message, user, skip_ocr=True)
+
+        self.assertEqual(
+            Document.objects.count(),
+            0
+        )
+
+
+def _create_email(
     attachment=None,
     maintype=None,
     subtype=None,
@@ -38,158 +228,3 @@ def create_email(
     msg.add_attachment(attachment, maintype=maintype, subtype=subtype)
     return msg.as_bytes()
 
-
-class TestIMAPImporterByUser(TestCase):
-    def setUp(self):
-        self.user = create_root_user()
-        self.margaret = create_margaret_user()
-        self.elizabet = create_elizabet_user()
-
-    @override_settings(PAPERMERGE_IMPORT_MAIL_BY_USER=True)
-    def test_extract_user_by_mail_address_not_allowed_user(self):
-        body = create_email(user=self.margaret)
-        email_message = email.message_from_bytes(
-            body, policy=email.policy.default)
-        user = extract_info_from_email(email_message)
-        self.assertIsNone(user)
-
-    @override_settings(PAPERMERGE_IMPORT_MAIL_BY_USER=True)
-    def test_extract_user_by_mail_address_allowed_user(self):
-        body = create_email(user=self.elizabet)
-        email_message = email.message_from_bytes(
-            body, policy=email.policy.default)
-        self.elizabet.mail_by_user = True
-        self.elizabet.save()
-        user = extract_info_from_email(email_message)
-        self.assertNotEqual(user, self.margaret)
-        self.assertEqual(user, self.elizabet)
-
-    def test_extract_user_by_mail_address_not_allowed_settings(self):
-        body = create_email(user=self.margaret)
-        email_message = email.message_from_bytes(
-            body, policy=email.policy.default)
-        user = extract_info_from_email(email_message)
-        self.assertIsNone(user)
-
-    def test_extract_user_by_mail_address_allowed_settings(self):
-        body = create_email(user=self.elizabet)
-        email_message = email.message_from_bytes(
-            body, policy=email.policy.default)
-        self.elizabet.mail_by_user = True
-        self.elizabet.save()
-        user = extract_info_from_email(email_message)
-        self.assertIsNone(user)
-
-
-class TestIMAPImporterBySecret(TestCase):
-    def setUp(self):
-        self.user = create_root_user()
-        self.margaret = create_margaret_user()
-        self.elizabet = create_elizabet_user()
-        self.margaret.mail_secret = 'this-is-margaret-secret'
-        self.margaret.save()
-        self.elizabet.mail_secret = 'this-is-elizabet-secret'
-        self.elizabet.save()
-
-    @override_settings(PAPERMERGE_IMPORT_MAIL_BY_SECRET=True)
-    def test_extract_user_by_secret_not_allowed_user(self):
-        body = create_email(subject='SECRET{this-is-margaret-secret}')
-        email_message = email.message_from_bytes(
-            body, policy=email.policy.default)
-        user = extract_info_from_email(email_message)
-        self.assertIsNone(user)
-
-    @override_settings(PAPERMERGE_IMPORT_MAIL_BY_SECRET=True)
-    def test_extract_user_by_secret_allowed_user(self):
-        body = create_email(subject='SECRET{this-is-elizabet-secret}')
-        email_message = email.message_from_bytes(
-            body, policy=email.policy.default)
-        self.elizabet.mail_by_secret = True
-        self.elizabet.save()
-        user = extract_info_from_email(email_message)
-        self.assertNotEqual(user, self.margaret)
-        self.assertEqual(user, self.elizabet)
-
-    def test_extract_user_by_secret_not_allowed_settings(self):
-        body = create_email(subject='SECRET{this-is-margaret-secret}')
-        email_message = email.message_from_bytes(
-            body, policy=email.policy.default)
-        user = extract_info_from_email(email_message)
-        self.assertIsNone(user, self.margaret)
-
-    def test_extract_user_by_secret_allowed_settings(self):
-        body = create_email(subject='SECRET{this-is-elizabet-secret}')
-        email_message = email.message_from_bytes(
-            body, policy=email.policy.default)
-        self.elizabet.mail_by_secret = True
-        self.elizabet.save()
-        user = extract_info_from_email(email_message)
-        self.assertIsNone(user)
-
-
-class TestIMAPImporterIngestion(TestCase):
-    def setUp(self):
-        self.user = create_root_user()
-
-    def test_pdf_imap_importer(self):
-        file_path = os.path.join(
-            BASE_DIR,
-            "data",
-            "berlin.pdf"
-        )
-        body = create_email(
-            attachment=file_path,
-            maintype='application',
-            subtype='pdf',
-        )
-        email_message = email.message_from_bytes(
-            body, policy=email.policy.default)
-        user = extract_info_from_email(email_message)
-        self.assertIsNone(user)
-        read_email_message(email_message, user, skip_ocr=True)
-        self.assertEqual(
-            Document.objects.count(),
-            1
-        )
-
-    def test_jpg_imap_importer(self):
-        file_path = os.path.join(
-            BASE_DIR,
-            "data",
-            "page-1.jpg"
-        )
-        body = create_email(
-            attachment=file_path,
-            maintype='image',
-            subtype='jpg',
-        )
-        email_message = email.message_from_bytes(
-            body, policy=email.policy.default)
-        user = extract_info_from_email(email_message)
-        self.assertIsNone(user)
-        read_email_message(email_message, user, skip_ocr=True)
-        self.assertEqual(
-            Document.objects.count(),
-            1
-        )
-
-    def test_tar_imap_importer(self):
-        file_path = os.path.join(
-            BASE_DIR,
-            "data",
-            "testdata.tar"
-        )
-        body = create_email(
-            attachment=file_path,
-            maintype='application',
-            subtype='x-tar',
-        )
-        email_message = email.message_from_bytes(
-            body, policy=email.policy.default)
-        user = extract_info_from_email(email_message)
-        self.assertIsNone(user)
-        read_email_message(email_message, user, skip_ocr=True)
-        self.assertEqual(
-            Document.objects.count(),
-            0
-        )
