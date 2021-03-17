240b3247a49b809cd725095fda4eb9fe4b29ae78
Eugen Ciur <eugen@papermerge.com>
2021-03-17 07:44:10 +0100

    wfeedback was renamed as leds

--- config/asgi.py
+++ config/asgi.py
@@ -3,7 +3,7 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import papermerge.wfeedback.routing
+import papermerge.leds.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.dev')
 
@@ -11,7 +11,7 @@ application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            papermerge.wfeedback.routing.websocket_urlpatterns
+            papermerge.leds.routing.websocket_urlpatterns
         )
     ),
     # Just HTTP for now. (We can add other protocols later.)
