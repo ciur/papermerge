41389b1659e2a5add1449d5df7722785b4846e23
Eugen Ciur <eugen@papermerge.com>
2021-11-20 08:13:54 +0100

    set default page size to 10

--- config/settings/base.py
+++ config/settings/base.py
@@ -358,7 +358,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'PAGE_SIZE': 5,
+    'PAGE_SIZE': 10,
     'EXCEPTION_HANDLER': 'rest_framework_json_api.exceptions.exception_handler',
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework_json_api.pagination.JsonApiPageNumberPagination',
@@ -426,4 +426,5 @@ DYNAMIC_PREFERENCES = {
 
 CORS_ALLOW_HEADERS = list(default_cors_headers) + [
     "Authorization",
+    "Content-Disposition"
 ]
